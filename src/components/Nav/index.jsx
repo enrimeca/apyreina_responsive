ed3ae41ef8aaaca02015e3cbfdc5cfda262b2ce0
Enrique Mejia <enriquemejiacano@gmail.com>
2021-03-14 12:31:49 -0500

    Adding mock information

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../../assets/images/logo.png'
 
-const Nav = () => {
+const Nav = ({options, color}) => {
+  const [hidden, setHidden] = useState(true);
+  const show = hidden? 'hidden' : '';
+
+  const toggleMenu = (event) => {
+    event.preventDefault();
+    setHidden(!hidden);
+  }
+
   return (
-    <nav>
-      
+    <nav className={`bg-${color}-600`}>
+      <h1>Soy un navegador</h1>
     </nav>
   )
 }
