a1fbbc189991f01c56ae482a074951e176815296
Enrique Mejia Cano <enrique.mejia@globant.com>
2021-03-15 21:21:18 -0500

    Adding postcss configuration

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -2,21 +2,22 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <section className="mx-6 md:mx-16 lg:mx-auto py-3 bg-white max-w-7xl">
+    <section className="mx-6 md:mx-16 lg:mx-auto py-3 bg-white max-w-4xl">
       <h1 className="section-title mt-12">Contacto</h1>
       <form action="">
         <label htmlFor="name">
-          <span>Nombres</span>
+          <span className="font-bold">NOMBRES</span>
           <input className="form-control" type="text" placeholder="Nombres" id="name" />
         </label>
         <label htmlFor="email">
-          <span>Email</span>
+          <span className="font-bold">EMAIL</span>
           <input className="form-control" type="email" placeholder="Correo electrónico" id="email" />
         </label>
         <label htmlFor="comment">
-          <span>Consulta</span>
+          <span className="font-bold">COMENTARIOS</span>
           <textarea className="form-control" name="" cols="30" rows="10" id="comment"></textarea>
-        </label>      
+        </label>
+        <button className="block bg-red-500 hover:bg-red-700 text-white uppercase text-lg mx-auto py-2 px-4 rounded my-4" type="submit">Enviar</button>
       </form>
     </section>
   )
